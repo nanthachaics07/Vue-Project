ad3319c6227be0ae9a6b4590e51b0d976d7df89a
Nanthachai.CS07 <botsafe002@gmail.com>
2024-02-01 16:59:05 +0700

    dev: form sumit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,54 @@
 <script setup>
-import {ref} from 'vue'
+import { reactive } from "vue"
 
+let formData = reactive({
+  firstname: "",
+  lastname: "",
+  age: 0,
+  gender: "",
+  interests: [],
+  description: "",
+})
+
+const gendersList = ['male', 'female', 'other']
+const interestsList = ['basketball', 'volleyball', 'tennis', 'badminton']
+
+const submitData = () => {
+  console.log('formData', JSON.parse(JSON.stringify(formData)))
+  console.log('interest', formData.interests.join())
+}
 </script>
 
 <template>
   <div>
-    <div>First Name</div>
-    <input type="text" v-model="firstName">
+    <div>
+      <div>First Name</div>
+      <input class="form" type="text" v-model="formData.firstname" />
+    </div>
+    <div>
+      <div>Last Name</div>
+      <input class="form" type="text" v-model="formData.lastname" />
+    </div>
+    <div>
+      <div>Age</div>
+      <input type="number" min="0" max="100" v-model="formData.age" />
+    </div>
+    <div>
+      <div>Gender</div>
+      <div v-for="gender in gendersList" v-bind:key="gender">
+        <input type="radio" :value="gender" v-model="formData.gender"> {{ gender }}
+      </div>
+    </div>
+    <div>
+      <div>Interests</div>
+      <div v-for="interest in interestsList" v-bind:key="interest">
+        <input type="checkbox" :value="interest" v-model="formData.interests"> {{ interest }}
+      </div>
+    </div>
+    <div>
+      <div>Description</div>
+      <textarea></textarea>
+    </div>
+    <button @click="submitData()">Submit</button>
   </div>
-  <div>
-    <div>Last Name</div>
-    <input type="text" v-model="LastName">
-  </div>
-  <div>
-    <div>Description</div>
-    <textarea></textarea>
-  </div>
-
 </template>
-
-<style scoped>
-
-</style>
