8d5def6794424478f10d7acedba6f9fe7eb36094
Nanthachai.CS07 <botsafe002@gmail.com>
2024-02-01 15:00:42 +0700

    dev: add create user type

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script setup>
 import {ref} from 'vue'
 
-const isCheck = ref(false)
-
-const toggle = () => {
-  isCheck.value = !isCheck.value
-}
-
-const answer = ref('')
-
 </script>
 
 <template>
   <div>
-    <div v-if ="answer === 'yes'">Answer's Yes</div>
-    <div v-else-if ="answer === 'no'">Answer's No</div>
-    <div v-else>No answer</div>
-    
-    <input type="radio" name="answer" value="yes" v-model="answer"> Yes
-    <input type="radio" name="answer" value="no" v-model="answer"> No
-    <!-- <button @click="toggle">Toggle</button> -->
+    <div>First Name</div>
+    <input type="text" v-model="firstName">
   </div>
+  <div>
+    <div>Last Name</div>
+    <input type="text" v-model="LastName">
+  </div>
+  <div>
+    <div>Description</div>
+    <textarea></textarea>
+  </div>
+
 </template>
+
+<style scoped>
+
+</style>
